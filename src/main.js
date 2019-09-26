a751176da28f980563d1b734c9577ed34295346e
happyfire <happyfire@gmail.com>
2019-09-26 22:27:48 +0800

    rollup starter app

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import update from './update.js';
+
+// even though Rollup is bundling all your files together, errors and
+// logs will still point to your original source modules
+console.log('if you have sourcemaps enabled in your devtools, click on main.js:5 -->');
+
+update();
